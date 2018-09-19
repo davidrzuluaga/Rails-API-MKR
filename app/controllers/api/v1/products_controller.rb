96f04e5623389dc136d2c4a5e7b5c53c2752c192
DavidRZuluaga <git@davidrzuluaga.com>
2018-09-19 14:43:17 -0500

    Crear productos con JSON

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,8 +1,23 @@
 class Api::V1::ProductsController < ApplicationController
     protect_from_forgery with: :null_session
 
-  def index
-    @product = Product.all
-    render json: @product
-  end
+    def index
+        @product = Product.all
+        render json: @product
+    end
+
+    def create
+        @product = Product.create(product_params)   
+        if @product.save
+          render json: @product
+        else
+          render json: { errors: @product.errors }, status: 422
+        end
+    end
+
+    private
+
+    def product_params
+      params.require(:product).permit(:name, :price)
+    end
 end
