70818b163c474190d9e7d7f7f74ec8bf2ec80fbe
DavidRZuluaga <git@davidrzuluaga.com>
2018-09-20 01:03:05 -0500

    idem.1

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
-      render json: @product, status: 201
+      render json: @product, status: 200
     else
       render json: { errors: @product.errors }, status: 422
     end
@@ -26,7 +26,7 @@ class Api::V1::ProductsController < ApplicationController
 
   def destroy
     @product = Product.find(params[:id])
-    @product.destroy
+    @product.destroy, status: 204
 
     head :no_content
   end
