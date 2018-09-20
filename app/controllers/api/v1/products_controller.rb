a27a150df23f78804fc3797548ba8ec05d4e5a12
DavidRZuluaga <git@davidrzuluaga.com>
2018-09-20 01:01:36 -0500

    Editar y eliminar productos con JSON

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -15,6 +15,22 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
 
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      render json: @product, status: 201
+    else
+      render json: { errors: @product.errors }, status: 422
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    head :no_content
+  end
+
 private 
 
   def product_params
