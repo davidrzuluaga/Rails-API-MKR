be5e202868c944996c54d7d6722ab566edabe657
davidrzuluaga <git@davidrzuluaga.com>
2018-09-19 19:45:10 -0500

    idem.1

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,18 +4,18 @@ class Api::V1::ProductsController < ApplicationController
     def index
         @product = Product.all
         render json: @product
-    end
-
-    def create
+      end
+      
+      def create
         @product = Product.create(product_params)   
         if @product.save
-          render json: @product
+          render json: { product: @product }, status: :created
         else
           render json: { errors: @product.errors }, status: 422
         end
     end
 
-    private
+  private
 
     def product_params
       params.require(:product).permit(:name, :price)
