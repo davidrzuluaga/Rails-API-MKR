6d55d7946be7a16722b77d9405da5206122b8ecd
DavidRZuluaga <git@davidrzuluaga.com>
2018-09-19 23:16:31 -0500

    idem.3

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,23 +1,24 @@
 class Api::V1::ProductsController < ApplicationController
-    protect_from_forgery with: :null_session
-
-    def index
-        @product = Product.all
-        render json: @product
-      end
-      
-      def create
-        @product = Product.create(product_params)   
-        if @product.save
-          render json: { product: @product }, status: :created
-        else
-          render json: { errors: @product.errors }, status: 422
-        end
+  protect_from_forgery with: :null_session
+ 
+  def index
+      @product = Product.all
+      render json: @product
+  end
+    
+  def create
+    @product = Product.create(product_params)   
+    if @product.save
+      render json: { product: @product }, status: :created
+    else
+      render json: { errors: @product.errors }, status: 422
     end
+  end
 
-  private 
+private 
 
-    def product_params
-      params.require(:product).permit(:name, :price)
-    end
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+
 end
